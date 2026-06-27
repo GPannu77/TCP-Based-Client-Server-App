cbdff02570034e521f1ed39bb11298f3c21c73df
Thebossnoor <gurnoor4771@gmail.com>
2026-06-27 18:50:59 -0400

    Completed processing request func, server.py is now complete

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 """
 server.py: Multi-Service TCP Server
 
-Using: python3 server.py <port>
+Using: python3 server.py 
+Port: 9000
 
 Listens to a specified port and handles each client
 connection in its own thread, so multiple clients can 
 be served simultaneously.
 """
-import socket, threading
+import socket
+import threading
 
 
 
@@ -20,7 +22,90 @@ server_socket.bind((HOST, PORT))
 server_socket.listen()
 
 def process_request(line):
-    pass
+    tokens = line.split()
+    
+    if len(tokens) != 5 or tokens[0].upper() != "CONVERT":
+        return "ERROR Malformed request"
+    
+    category = tokens[1].upper()
+    from_type = tokens[2].upper()
+    to_type = tokens[3].upper()
+    value_str = tokens[4]
+    
+    try:
+        value = float(value_str)
+    except ValueError:
+        return "ERROR Invalid numeric value"
+    
+    if category == "TEMP":
+        if from_type == to_type:
+            result = value
+        elif from_type == "C" and to_type =="F":
+            result = (value * 9/5) + 32
+        elif from_type == "F" and to_type == "C":
+            result = (value - 32) * 5/9
+        else:
+            return "ERROR Invalid unit"
+        return f"RESULT {result:.2f} {to_type}"
+    
+    elif category == "CURRENCY":
+        
+        if from_type == to_type:
+            result = value
+        elif from_type == "USD" and to_type == "EUR":
+            result = value * 0.88
+        elif from_type == "EUR" and to_type == "USD":
+            result = value * 1.14
+        elif from_type == "USD" and to_type == "GBP":
+            result = value * 0.76
+        elif from_type == "GBP" and to_type == "USD":
+            result = value * 1.32
+        elif from_type == "USD" and to_type == "CAD":
+            result = value * 1.37
+        elif from_type == "CAD" and to_type == "USD":
+            result = value * 0.73
+        elif from_type == "EUR" and to_type == "GBP":
+            result = value * 0.86
+        elif from_type == "GBP" and to_type == "EUR":
+            result = value * 1.16
+        elif from_type == "EUR" and to_type == "CAD":
+            result = value * 1.62
+        elif from_type == "CAD" and to_type == "EUR":
+            result = value * 0.62
+        elif from_type == "GBP" and to_type == "CAD":
+            result = value * 1.87
+        elif from_type == "CAD" and to_type == "GBP":
+            result = value * 0.53
+        else:
+            return "ERROR Invalid unit"
+        return f"RESULT {result:.2f} {to_type}"
+    
+    elif category == "LENGTH":
+        
+        if from_type == to_type:
+            result = value
+        elif from_type == "KM" and to_type == "MI":
+            result = value * 0.621371
+        elif from_type == "MI" and to_type == "KM":
+            result = value * 1.60934
+        else:
+            return "ERROR Invalid unit"
+        return f"RESULT {result:.2f} {to_type}"
+    
+    elif category == "WEIGHT":
+        
+        if from_type == to_type:
+            result = value
+        elif from_type == "KG" and to_type == "LB":
+            result = value * 2.20462
+        elif from_type == "LB" and to_type == "KG":
+            result = value * 0.453592
+        else:
+            return "ERROR Invalid unit"
+        return f"RESULT {result:.2f} {to_type}"
+    else:
+        return "ERROR Invalid category"
+        
 
 def handle_client(connection, address):
     print(f"Connected by {address}")
@@ -39,6 +124,7 @@ def handle_client(connection, address):
     
     connection.close()
     print(f"Connection with {address} now closed.")
+
 
 def main():
     print(f"Server listening on port {PORT}...")
